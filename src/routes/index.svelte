f7b97340c97f51335f8a48aa56ffe11b59ee8615
Osman Coskun <44034936+osmancoskun@users.noreply.github.com>
2023-05-11 10:06:31 +0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <main
-    class="h-screen w-screen flex flex-col gap-y-5 items-center justify-center p-10 bg-base-100 border"
+    class="h-screen w-screen flex flex-col gap-y-5 items-center justify-center p-10 border"
 >
-    <a href="#" class="font-bold text-2xl"> ROUTIFY + SVELTE + MARKDOWN </a>
-    <a href="/hello-world">Hello World markdown page</a>
-    <a href="https://google.com">Google Deneme</a>
+    <div class=" p-10 border bg-gray-100">
+        <a href="#" class="font-bold text-2xl"> ROUTIFY + SVELTE + MARKDOWN </a>
+        <a href="/hello-world">Hello World markdown page</a>
+        <a href="https://google.com">Google Deneme</a>
+    </div>
 </main>

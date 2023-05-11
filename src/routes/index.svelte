1d7e892f15a9c34bcc478f6c8c7621cc9ef636a5
Osman Coskun <44034936+osmancoskun@users.noreply.github.com>
2023-05-11 09:50:26 +0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <main
-    class="h-screen w-screen flex flex-col gap-y-5 items-center justify-center"
+    class="h-screen w-screen flex flex-col gap-y-5 items-center justify-center p-10 bg-base-100 border"
 >
     <a href="#" class="font-bold text-2xl"> ROUTIFY + SVELTE + MARKDOWN </a>
     <a href="/hello-world">Hello World markdown page</a>
